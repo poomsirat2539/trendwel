d0e3df8b7046820f19903be0e31f2383f4deb381
poomsirat2539 <74708776+poomsirat2539@users.noreply.github.com>
2026-01-31 16:01:57 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-const slides = document.querySelectorAll('.hero-slide');
+const slides = document.querySelectorAll(".slide");
 let current = 0;
 
 setInterval(() => {
-  slides[current].classList.remove('active');
+  slides[current].classList.remove("active");
   current = (current + 1) % slides.length;
-  slides[current].classList.add('active');
-}, 4000);
+  slides[current].classList.add("active");
+}, 3000);
